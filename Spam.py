5284c88c10f9ca43879358091a5e5123b267af59
BasiliskFTW <prith.tiu13@gmail.com>
2022-07-03 06:39:34 +0530

    Programs

--- Spam.py
+++ Spam.py
@@ -3,7 +3,6 @@ import time
 import webbrowser as wb
 wb.open("web.whatsapp.com")
 time.sleep(1)
-for i in range(10):
-    pg.write("XD\n")
+for i in range(100):
+    pg.write("Happy Birthday Fatima\n")
     pg.press("Enter")
-
